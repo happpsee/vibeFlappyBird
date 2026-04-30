4896967f6a2a1a818297c1bd0ff7c11152dc710c
userName <userEmail>
2026-04-30 21:11:40 +0800

    feat:complete

--- js/config.js
+++ js/config.js
@@ -6,21 +6,34 @@
  * @FilePath: \vibeProject\js\config.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
+
+// 图片资源导入
+import imgBgCover from '../assets/images/bgc.png';
+import imgBgDay from '../assets/images/bg_day.png';
+import imgBgNight from '../assets/images/bg_night.png';
+import imgTitle from '../assets/images/title.png';
+import imgBtnPlay from '../assets/images/button_play.png';
+import imgBird from '../assets/images/bird.png';
+import imgPipeUp from '../assets/images/pipe_up.png';
+import imgPipeDown from '../assets/images/pipe_down.png';
+import imgBtnPause from '../assets/images/button_pause.png';
+import imgBtnResume from '../assets/images/button_resume.png';
+
 // 游戏配置常量
 export const CANVAS_WIDTH = 320;
 export const CANVAS_HEIGHT = 480;
 
 // 图片资源路径
-export const IMG_BG_COVER = '../assets/images/bgc.png';
-export const IMG_BG_DAY = '../assets/images/bg_day.png';
-export const IMG_BG_NIGHT = '../assets/images/bg_night.png';
-export const IMG_TITLE = '../assets/images/title.png';
-export const IMG_BTN_PLAY = '../assets/images/button_play.png';
-export const IMG_BIRD = '../assets/images/bird.png';
-export const IMG_PIPE_UP = '../assets/images/pipe_up.png';
-export const IMG_PIPE_DOWN = '../assets/images/pipe_down.png';
-export const IMG_BTN_PAUSE = '../assets/images/button_pause.png';
-export const IMG_BTN_RESUME = '../assets/images/button_resume.png';
+export const IMG_BG_COVER = imgBgCover;
+export const IMG_BG_DAY = imgBgDay;
+export const IMG_BG_NIGHT = imgBgNight;
+export const IMG_TITLE = imgTitle;
+export const IMG_BTN_PLAY = imgBtnPlay;
+export const IMG_BIRD = imgBird;
+export const IMG_PIPE_UP = imgPipeUp;
+export const IMG_PIPE_DOWN = imgPipeDown;
+export const IMG_BTN_PAUSE = imgBtnPause;
+export const IMG_BTN_RESUME = imgBtnResume;
 
 // 昼夜判断阈值（小时，24 小时制）
 export const DAY_START_HOUR = 6;
