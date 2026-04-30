40e5d27484f349195fad403986694fed702276a5
userName <userEmail>
2026-04-30 21:02:23 +0800

    feat:complete

--- js/config.js
+++ js/config.js
@@ -1,18 +1,26 @@
+/*
+ * @Author: userName userEmail
+ * @Date: 2026-04-30 20:45:14
+ * @LastEditors: userName userEmail
+ * @LastEditTime: 2026-04-30 21:02:15
+ * @FilePath: \vibeProject\js\config.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 // 游戏配置常量
 export const CANVAS_WIDTH = 320;
 export const CANVAS_HEIGHT = 480;
 
 // 图片资源路径
-export const IMG_BG_COVER = '/assets/images/bgc.png';
-export const IMG_BG_DAY = '/assets/images/bg_day.png';
-export const IMG_BG_NIGHT = '/assets/images/bg_night.png';
-export const IMG_TITLE = '/assets/images/title.png';
-export const IMG_BTN_PLAY = '/assets/images/button_play.png';
-export const IMG_BIRD = '/assets/images/bird.png';
-export const IMG_PIPE_UP = '/assets/images/pipe_up.png';
-export const IMG_PIPE_DOWN = '/assets/images/pipe_down.png';
-export const IMG_BTN_PAUSE = '/assets/images/button_pause.png';
-export const IMG_BTN_RESUME = '/assets/images/button_resume.png';
+export const IMG_BG_COVER = '../assets/images/bgc.png';
+export const IMG_BG_DAY = '../assets/images/bg_day.png';
+export const IMG_BG_NIGHT = '../assets/images/bg_night.png';
+export const IMG_TITLE = '../assets/images/title.png';
+export const IMG_BTN_PLAY = '../assets/images/button_play.png';
+export const IMG_BIRD = '../assets/images/bird.png';
+export const IMG_PIPE_UP = '../assets/images/pipe_up.png';
+export const IMG_PIPE_DOWN = '../assets/images/pipe_down.png';
+export const IMG_BTN_PAUSE = '../assets/images/button_pause.png';
+export const IMG_BTN_RESUME = '../assets/images/button_resume.png';
 
 // 昼夜判断阈值（小时，24 小时制）
 export const DAY_START_HOUR = 6;
--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: userName userEmail
+ * @Date: 2026-04-30 13:47:30
+ * @LastEditors: userName userEmail
+ * @LastEditTime: 2026-04-30 20:55:44
+ * @FilePath: \vibeProject\vite.config.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
